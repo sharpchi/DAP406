e1d730e3239baaab3963d463a31cf1fc775ef8be
Mark Sharp <m.sharp@chi.ac.uk>
2017-08-19 20:35:16 +0100

    index page and templates

--- inc/db.php
+++ inc/db.php
@@ -1,10 +1,8 @@
 <?php
 
-require_once('../config.php');
-
 class db {
-    public var $conn;
-    
+    public $conn;
+
     public function __construct() {
         global $CFG;
         $dsn = 'mysql:dbname=' . $CFG->dbname . ';host=' . $CFG->dbhost;
@@ -14,31 +12,31 @@ class db {
             echo 'Connection failed: ' . $e->getMessage();
         }
     }
-    
+
     public function insertBook($title, $authors, $genres, $yearpublished, $publisherid, $isbn) {
         global $DB;
         $sql = "INSERT into `book` (`title`,`yearpublished`,`isbn`,`publisher`) VALUES (:title, :yearpublished, :isbn, :publisher)";
         $stmt = $this->conn->prepare($sql);
-        
+
         $stmt->bindValue(':title', $title, PDO::PARAM_STR);
         $stmt->bindValue(':yearpublished', $yearpublished, PDO::PARAM_INT);
         $stmt->bindValue(':isbn', $isbn, PDO::PARAM_STR);
         $stmt->bindValue(':publisherid', $publisherid, PDO::PARAM_INT);
-        
+
         $stmt->execute();
         return $this->conn->lastInsertId();
     }
-    
+
     public function insertPublisher($name, $address1, $address2, $town, $county, $country, $postcode, $phone, $www, $twitter) {
         global $DB;
-        
-        $sql = "INSERT INTO `publisher` 
-            (`name`, `address1`, `address2`, `town`, `county`, `country`, `postcode`, `phone`, `www`, `twitter`) 
-            VALUES 
+
+        $sql = "INSERT INTO `publisher`
+            (`name`, `address1`, `address2`, `town`, `county`, `country`, `postcode`, `phone`, `www`, `twitter`)
+            VALUES
             (:name, :address1, :address2, :town, :county, :country, :postcode, :phone, :www, :twitter)";
-        
+
         $stmt = $this->conn->prepare($sql);
-        
+
         $stmt->bindValue(':name', $name, PDO::PARAM_STR);
         $stmt->bindValue(':address1', $address1, PDO::PARAM_STR);
         $stmt->bindValue(':address2', $address2, PDO::PARAM_STR);
@@ -49,57 +47,57 @@ class db {
         $stmt->bindValue(':phone', $phone, PDO::PARAM_STR);
         $stmt->bindValue(':www', $www, PDO::PARAM_STR);
         $stmt->bindValue(':twitter', $twitter, PDO::PARAM_STR);
-        
+
         $stmt->execute();
-        
-        return $this->conn->lastInsertId();       
-        
+
+        return $this->conn->lastInsertId();
+
     }
-    
+
     public function insertAuthor($firstname, $lastname, $www, $twitter) {
         global $DB;
-        
-        $sql = "INSERT INTO `author` 
-            (`firstname`, `lastname`, `www`, `twitter`) 
-            VALUES 
+
+        $sql = "INSERT INTO `author`
+            (`firstname`, `lastname`, `www`, `twitter`)
+            VALUES
             (:firstname, :lastname, :www, :twitter)";
-        
+
         $stmt = $this->conn->prepare($sql);
-        
+
         $stmt->bindValue(':firstname', $firstname, PDO::PARAM_STR);
         $stmt->bindValue(':lastname', $lastname, PDO::PARAM_STR);
         $stmt->bindValue(':www', $www, PDO::PARAM_STR);
         $stmt->bindValue(':twitter', $twitter, PDO::PARAM_STR);
-        
+
         $stmt->execute();
-        
-        return $this->conn->lastInsertId();       
-        
+
+        return $this->conn->lastInsertId();
+
     }
-    
+
     public function updateBook($id, $title, $authors, $genres, $yearpublished, $publisherid, $isbn) {
         global $DB;
         $sql = "UPDATE `book` SET (`title`=:title,`yearpublished`=:yearpublished,`isbn`=:isbn,`publisher`=:publisher) WHERE `id`=:id";
         $stmt = $this->conn->prepare($sql);
-        
+
         $stmt->bindValue(':title', $title, PDO::PARAM_STR);
         $stmt->bindValue(':yearpublished', $yearpublished, PDO::PARAM_INT);
         $stmt->bindValue(':isbn', $isbn, PDO::PARAM_STR);
         $stmt->bindValue(':publisherid', $publisherid, PDO::PARAM_INT);
         $stmt->bindValue(':id', $id, PDO::PARAM_INT);
-        
+
         $stmt->execute();
-        
+
     }
-    
+
     public function updatePublisher($id, $name, $address1, $address2, $town, $county, $country, $postcode, $phone, $www, $twitter) {
         global $DB;
-        
+
         $sql = "UPDATE `publisher` SET
             (`name`=:name, `address1`=:address1, `address2`:=address2, `town`=:town, `county`=:county, `country`=:country, `postcode`=:postcode, `phone`=:phone, `www`=:www, `twitter`=:twitter)";
-        
+
         $stmt = $this->conn->prepare($sql);
-        
+
         $stmt->bindValue(':name', $name, PDO::PARAM_STR);
         $stmt->bindValue(':address1', $address1, PDO::PARAM_STR);
         $stmt->bindValue(':address2', $address2, PDO::PARAM_STR);
@@ -111,27 +109,27 @@ class db {
         $stmt->bindValue(':www', $www, PDO::PARAM_STR);
         $stmt->bindValue(':twitter', $twitter, PDO::PARAM_STR);
         $stmt->bindValue(':id', $id, PDO::PARAM_INT);
-        
+
         $stmt->execute();
-               
-        
+
+
     }
-    
+
     public function updateAuthor($id, $firstname, $lastname, $www, $twitter) {
         global $DB;
-        
+
         $sql = "UPDATE `author` SET
             (`firstname`=:firstname, `lastname`=:lastname, `www`=:www, `twitter`=:twitter)";
-        
+
         $stmt = $this->conn->prepare($sql);
-        
+
         $stmt->bindValue(':firstname', $firstname, PDO::PARAM_STR);
         $stmt->bindValue(':lastname', $lastname, PDO::PARAM_STR);
         $stmt->bindValue(':www', $www, PDO::PARAM_STR);
         $stmt->bindValue(':twitter', $twitter, PDO::PARAM_STR);
         $stmt->bindValue(':id', $id, PDO::PARAM_INT);
-        
+
         $stmt->execute();
-        
+
     }
 }
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php 
-require('config.php');
+<?php
+include('inc/header.php');
+?>
+    <h1>Mark's books</h1>
+    <form method="post" action="">
+        <label for="search">Search books</label>
+        <input name="search" value="" type="text" placeholder="Search books" />
+        <input type="submit" value="Search" name="search" />
+    </form>
 
+<?php
+include('inc/footer.php');
