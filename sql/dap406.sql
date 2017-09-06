6e656304019cfe035be82893a906ceb4eaaf3f2b
Mark Sharp <m.sharp@chi.ac.uk>
2017-09-06 10:16:36 +0100

    updating database

--- sql/dap406.sql
+++ sql/dap406.sql
@@ -1,103 +1,115 @@
--- phpMyAdmin SQL Dump
--- version 4.7.3
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 5.7.19, for Linux (x86_64)
 --
--- Host: localhost
--- Generation Time: Sep 06, 2017 at 04:36 AM
--- Server version: 10.2.7-MariaDB-log
--- PHP Version: 7.1.8
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: MSharp
+-- ------------------------------------------------------
+-- Server version	5.7.19-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `dap406`
---
-
--- --------------------------------------------------------
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `author`
 --
 
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `author` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `firstname` varchar(50) NOT NULL,
   `lastname` varchar(50) NOT NULL,
   `email` varchar(100) DEFAULT NULL,
   `www` varchar(150) DEFAULT NULL,
-  `twitter` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `twitter` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `author_name` (`firstname`,`lastname`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `author`
 --
 
-INSERT INTO `author` (`id`, `firstname`, `lastname`, `email`, `www`, `twitter`) VALUES
-(1, 'Jane', 'Seale', 'j.seale@routledge.com', 'http://www.jseale.com', '@jseale'),
-(2, 'Shaun R.', 'Harper', '', '', ''),
-(3, 'Stephen John', 'Quaye', 'sj@quaye.com', 'http://www.quaye.com', ''),
-(4, 'Fred', 'Simmons', '', '', '');
-
--- --------------------------------------------------------
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+INSERT INTO `author` VALUES (1,'Jane','Seale','j.seale@routledge.com','http://www.jseale.com','@jseale'),(2,'Shaun R.','Harper','','',''),(3,'Stephen John','Quaye','sj@quaye.com','http://www.quaye.com',''),(4,'Fred','Simmons','','',''),(9,'Mark','Tadajewski','','',''),(10,'Douglas','Brownlie','','',''),(11,'Ray','Wright','','','');
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `book`
 --
 
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `book` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `yearpublished` int(4) NOT NULL,
   `isbn` varchar(20) NOT NULL,
-  `publisherid` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `publisherid` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `book_title` (`title`),
+  KEY `fk_book_publisher` (`publisherid`),
+  CONSTRAINT `fk_book_publisher` FOREIGN KEY (`publisherid`) REFERENCES `publisher` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `book`
 --
 
-INSERT INTO `book` (`id`, `title`, `yearpublished`, `isbn`, `publisherid`) VALUES
-(3, 'E-Learning and Disability in Higher Education - Accessibility research and practice', 2006, '0414383102', 11),
-(4, 'Student Engagement in Higher Education - Theoretical Perspectives and Practical Approaches for Diverse Populations', 2009, '9780415988513', 11);
-
--- --------------------------------------------------------
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (3,'E-Learning and Disability in Higher Education - Accessibility research and practice',2006,'0414383102',11),(4,'Student Engagement in Higher Education - Theoretical Perspectives and Practical Approaches for Diverse Populations',2009,'9780415988513',11),(5,'Critical Marketing',2008,'9780470511955',12),(6,'Consumer Behaviour',2006,'1844801381',13);
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `book_author`
 --
 
+DROP TABLE IF EXISTS `book_author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `book_author` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `bookid` int(10) UNSIGNED NOT NULL,
-  `authorid` int(10) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `bookid` int(10) unsigned NOT NULL,
+  `authorid` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `book_author` (`bookid`,`authorid`) USING BTREE,
+  KEY `fk_ba_author` (`authorid`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `book_author`
 --
 
-INSERT INTO `book_author` (`id`, `bookid`, `authorid`) VALUES
-(24, 3, 1),
-(25, 4, 2),
-(26, 4, 3);
-
--- --------------------------------------------------------
+LOCK TABLES `book_author` WRITE;
+/*!40000 ALTER TABLE `book_author` DISABLE KEYS */;
+INSERT INTO `book_author` VALUES (24,3,1),(25,4,2),(26,4,3),(30,5,9),(31,5,10),(32,6,11);
+/*!40000 ALTER TABLE `book_author` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `publisher`
 --
 
+DROP TABLE IF EXISTS `publisher`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `publisher` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `address1` varchar(255) DEFAULT NULL,
   `address2` varchar(255) DEFAULT NULL,
@@ -107,86 +119,29 @@ CREATE TABLE `publisher` (
   `postcode` varchar(255) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
   `www` varchar(100) DEFAULT NULL,
-  `twitter` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `twitter` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `publisher_name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `publisher`
 --
 
-INSERT INTO `publisher` (`id`, `name`, `address1`, `address2`, `town`, `county`, `country`, `postcode`, `phone`, `www`, `twitter`) VALUES
-(1, 'Penguin', '1 Fish Street', '', '', 'London', 'GB', 'SE1 1EE', '020 7555 5555', 'http://www.penguin.com', '@penguin'),
-(11, 'Routledge', '2 Park Square', 'Milton Park', 'Abingdon', 'Oxfordshire', 'GB', 'OX14 4RN', '', 'http://www.routledge.com', '@routledgebooks');
+LOCK TABLES `publisher` WRITE;
+/*!40000 ALTER TABLE `publisher` DISABLE KEYS */;
+INSERT INTO `publisher` VALUES (1,'Penguin','1 Fish Street','','','London','GB','SE1 1EE','020 7555 5555','http://www.penguin.com','@penguin'),(11,'Routledge','2 Park Square','Milton Park','Abingdon','Oxfordshire','GB','OX14 4RN','','http://www.routledge.com','@routledgebooks'),(12,'John Wiley & Sons','The Atrium','Southern Gate','Chichester','West Sussex','GB','PO19 8SQ','01243 779777','http://www.wiley.com','@wileyglobal'),(13,'Thomson','High Holborn House','60-51 Bedford Row','London','','GB','WC1R 4LR','','http://www.thomsonlearning.co.uk','');
+/*!40000 ALTER TABLE `publisher` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `author`
---
-ALTER TABLE `author`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `author_name` (`firstname`,`lastname`) USING BTREE;
-
---
--- Indexes for table `book`
---
-ALTER TABLE `book`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `book_title` (`title`),
-  ADD KEY `fk_book_publisher` (`publisherid`);
-
---
--- Indexes for table `book_author`
---
-ALTER TABLE `book_author`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `book_author` (`bookid`,`authorid`) USING BTREE,
-  ADD KEY `fk_ba_author` (`authorid`);
-
---
--- Indexes for table `publisher`
---
-ALTER TABLE `publisher`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `publisher_name` (`name`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `author`
---
-ALTER TABLE `author`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
---
--- AUTO_INCREMENT for table `book`
---
-ALTER TABLE `book`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `book_author`
---
-ALTER TABLE `book_author`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
---
--- AUTO_INCREMENT for table `publisher`
---
-ALTER TABLE `publisher`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `book`
---
-ALTER TABLE `book`
-  ADD CONSTRAINT `fk_book_publisher` FOREIGN KEY (`publisherid`) REFERENCES `publisher` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-09-06 10:15:28
