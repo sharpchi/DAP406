1e4b411e323cdbca83879dbe8b8d44541c88db6c
Mark Sharp <m.sharp@chi.ac.uk>
2017-09-12 18:37:01 +0100

    101

--- broken/index.php
+++ broken/index.php
@@ -1,11 +1,41 @@
 <?php
 
-$string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-$int = 123;
-$object = new stdClass();
-$object->name = "Fred";
-$object->value = $string;
-$array = array('name' => 'Fred', 'value' => $string);
+helpme($topic);
 
-echo 'Some text plus my $string';
-echo "Some text plus my $string";
+function he1pme($topic) {
+    echo "<p>Help topic: $topic.</p>"
+}
+
+$message1 = 'This isn't my day';
+
+$person = stdClass();
+$person->name = 'Jane';
+$person->qualifications = ['A level English'; 'GCSE Maths'];
+
+$people[] = $person;
+
+$person = stdClass();
+$person->name = 'Rod';
+$person->qualifications = ['A level Spanish'; 'GCSE English'];
+
+$people[] = $person;
+
+for each ($p as People) {
+    echo 'Name: {$p->name}<br />';
+    echo 'Qualifications: {$p->qualifications}<br />';
+    echo 'Job: {$p->jobtitle}<br />';
+}
+
+// Clue: search php PDO
+$DB = new PDO('mysql:dbname=MSharp;host=localhost', 'MSharp', '123456789');
+// Clue: search PDO setAttribute
+$sql = "SELECT a.* FROM 'author' WHERE fistname LIKE 'jan'";
+
+$stmt = $DB->query($sql);
+
+$results = $stmt->fetchAll();
+foreach ($results as $item) {
+    foreach ($item as $key -> $data) {
+        echo "{$key}: {$data}<br />";
+    }
+}
--- php101/4_forms.php
+++ php101/4_forms.php
@@ -1,5 +1,5 @@
 <h1>Forms</h1>
-<p>A webpage can receive data from another page by a couple of methods: 1. the url 2: as form data.</p>
+<p>A webpage can receive data from another page by a couple of methods: 1. the url 2: as form data. Both these methods produce an array, you access as with any other named array.</p>
 <h2>$_GET</h2>
 <p>When you see a url like: <code>http://localhost/DAP406/php101/4_forms.php?query=fred&page=1</code>, the variables query and page are processed using the $_GET array.</p>
 <p><a href="4_forms.php?query=fred&page=1">Click on this link.</a></p>
@@ -17,6 +17,13 @@ if (isset($_GET)) {
 ?>
 <h2>$_POST</h2>
 <p>Usually, data sent from a form use the POST method (you can use GET, but POST is more usual)</p>
+<pre><code>
+    &lt;form name="testform" method="POST" action="4_forms.php">
+        &lt;label for="query">Query:&lt;/label> &lt;input type="text" name="query" value="Fred" />&lt;br />
+        &lt;label for="page">Page:&lt;/label> &lt;input type="number" name="page" value=1 />&lt;/br />
+        &lt;input type="submit" value="Submit" />
+    &lt;/form>
+</code></pre>
 <p>Submit this form</p>
 <form name="testform" method="POST" action="4_forms.php">
     <label for="query">Query:</label> <input type="text" name="query" value="Fred" /><br />
